2078b62c006d7cd029315d75215dcba7934bde5e
Petronerd1 <bedirhanttepe@gmail.com>
2021-12-08 17:13:42 +0300

    Blog2.3
    Blog2.3

---
+++ CoreDemo/CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EFWriterRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
+        {
+            return View();
+
+        }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavBarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+
+        }
+        [AllowAnonymous]
+        public IActionResult WriterEditProfile()
+        {
+            var writervalues = wm.TGetById(1);
+            return View(writervalues);
+        }
+    }
+}
